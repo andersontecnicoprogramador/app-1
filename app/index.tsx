c6c08e53d060dc6ff0b8b9971000c94d8ba23e84
Anderson Moreira <anderson.tecnicoprogramdor@hotmail.com>
2024-06-28 16:12:34 -0300

    primeiro commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View , Image} from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Primeiro App React-Native.</Text>
+      <Text>Meu nome é Anderson.</Text>
+      <Text>e eu curso senac.</Text>
+    </View>
+  );
+}
