f53b3511cecbfe8b67c9f3cd5eeab93d47dc98e3
Anderson Moreira <anderson.tecnicoprogramdor@hotmail.com>
2024-06-28 17:18:08 -0300

    segundo commit

--- app/index.tsx
+++ app/index.tsx
@@ -3,15 +3,23 @@ import { Text, View , Image} from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
     >
+      <Image source={require('../assets/images/react-logo.png')}/>
+      <Image 
+      style={{width:300,
+        height:300,
+      borderRadius:300}}
+      source={require('../assets/images/02.png')}/>
+      
       <Text>Primeiro App React-Native.</Text>
       <Text>Meu nome é Anderson.</Text>
       <Text>e eu curso senac.</Text>
     </View>
   );
 }
+
